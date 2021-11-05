98d2acd171d1a74fe1dc28576b947cca4a677cc3
Stanleee <Stanz.ferigh@gmail.com>
2021-11-05 17:45:00 +1100

    iss icon location based on lat and lng added to leaflet map

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ apod.getData();
 marsPhotos.getData();
 search.getData();
 
+// Realtime ISS Location
 const mymap = L.map('issMap').setView([0, 0], 2);
 const attribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
@@ -18,17 +19,20 @@ const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl, { attribution });
 tiles.addTo(mymap);
 
-const marker = L.marker([0, 0])
-  .addTo(mymap)
-  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-  .openPopup();
+const issIcon = L.icon({
+  iconUrl: './images/iss200.png',
+  iconSize: [50, 32],
+  iconAnchor: [25, 16],
+});
 
-const getISS = async () => {
+const marker = L.marker([0, 0], { icon: issIcon }).addTo(mymap);
+
+async function getISS() {
   const res = await fetch(`https://api.wheretheiss.at/v1/satellites/25544`);
   const data = await res.json();
   console.log(data.latitude, data.longitude);
   marker.setLatLng([data.latitude, data.longitude]);
-};
+}
 
 getISS();
 setInterval(getISS, 1000);
